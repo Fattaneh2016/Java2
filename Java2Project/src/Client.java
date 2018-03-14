6f1f880381705e548fbb4b10b14ee51e068a2870
fatameri@hotmail.com <amir4767>
2018-03-14 08:56:18 -0400

    as

--- Java2Project/src/Client.java
+++ Java2Project/src/Client.java
@@ -3,7 +3,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi there all all");
+System.out.println("hi there all all all");
 	}
 
 }
